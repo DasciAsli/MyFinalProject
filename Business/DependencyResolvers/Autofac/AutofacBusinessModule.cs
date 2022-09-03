6a02887ba2eb1d80958fc6718b4c57f6085b5e9a
AsliDasci <aslihandasci1@gmail.com>
2022-09-03 17:59:45 +0300

    Ders12-Autofac Configuration

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)//Uygulama yayınlandığında çalışır.
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            //Birisi IProductService isterse ona bir tane ProductManager instance'ı ver
+            builder.RegisterType<EFProductDal>().As<IProductDal>().SingleInstance();
+
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -21,8 +24,21 @@ internal class Program
         //Autofac,Ninject,CastleWindsor,StructureMap,LightInject,DryInject bunlarýn hepsi  IoC yapýsý yokken IoC yerine alt yapý sunuyordu
         //Biz Aop yapmayý planlýyoruz.AOP bir metodun önünde,sonunda,metod hata verdiðinde çalýþan kod parçacýklarýný yazmamýzý saðlar
         //Autofac bize Aop imkaný sunuyor.O yüzden Autofac'i enjekte edeceðiz daha sonra
-        builder.Services.AddSingleton<IProductService,ProductManager>(); //Bana arka planda bir referans oluþtur.Yani IoC'ler bizim yerimize newliyor.Bellekte bir tane ProductManager oluþturuyor.Ne kadar client gelirse gelsin hepsine ayný ProductManager'ý veriyor.Ancak içerisinde data olmamalý
-        builder.Services.AddSingleton<IProductDal, EFProductDal>();
+
+        //builder.Services.AddSingleton<IProductService,ProductManager>(); //Bana arka planda bir referans oluþtur.Yani IoC'ler bizim yerimize newliyor.Bellekte bir tane ProductManager oluþturuyor.Ne kadar client gelirse gelsin hepsine ayný ProductManager'ý veriyor.Ancak içerisinde data olmamalý
+        //builder.Services.AddSingleton<IProductDal, EFProductDal>();
+
+        //AOP'ye geçtikten sonra Businessda iþlemlerimizi yaptýk
+        //Autofac için gerekli konfigürasyon
+         builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+         .ConfigureContainer<ContainerBuilder>(builder =>
+         {
+            builder.RegisterModule(new AutofacBusinessModule());
+         });
+
+
+
+
         var app = builder.Build();  
 
         // Configure the HTTP request pipeline.
