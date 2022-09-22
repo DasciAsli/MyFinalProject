3bcc513c43654d44df026dbe90d752bee3d9daf1
AsliDasci <aslihandasci1@gmail.com>
2022-09-22 13:47:35 +0300

    Ders15-CacheAspect

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using Business.BusinessAspect.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -38,6 +39,7 @@ namespace Business.Concrete
 
         [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))] //Add metodunu ProductValidatordeki kurallara göre doğrula
+        [CacheRemoveAspect("IProductService.Get")]//IProductServicede içinde Get geçen tüm cacheleri siler.Data değiştiği için
         public IResult Add(Product product)
         {
 
@@ -70,6 +72,7 @@ namespace Business.Concrete
 
         }
 
+        [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
             if (DateTime.Now.Hour == 24)
@@ -85,6 +88,8 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id)) ;
         }
 
+
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
---
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -0,0 +1,43 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Caching;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Caching
+{
+    public class CacheAspect : MethodInterception
+    {
+        private int _duration;
+        private ICacheManager _cacheManager;
+
+        public CacheAspect(int duration = 60)//Süre vermezsek 60dk boyunca duracak veri cachede ve sonra atılacak
+        {
+            _duration = duration;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        public override void Intercept(IInvocation invocation) //OnBefore'da kullanabilirdik farketmez
+        {
+            //Methodun ismini bul.ReflectedType.FullName demek namespace + classın ismini al
+            //Burada key oluşturmaya çalışıyorum
+            var methodName = string.Format($"{invocation.Method.ReflectedType.FullName}.{invocation.Method.Name}");
+            var arguments = invocation.Arguments.ToList();
+            //metodun parametrelerini listeye çevir
+            var key = $"{methodName}({string.Join(",", arguments.Select(x => x?.ToString() ?? "<Null>"))})";
+            //string.Join birleştirme için kullanılıyor
+            if (_cacheManager.IsAdd(key)) //bellekte böyle bir anahtar var mı kontrol et
+            {
+                invocation.ReturnValue = _cacheManager.Get(key); //metodu çalıştırmadan geri dön
+                return;
+            }
+            invocation.Proceed();
+            _cacheManager.Add(key, invocation.ReturnValue, _duration);
+        }
+    }
+}
